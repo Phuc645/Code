1823c3496c8d33ee194e7cf175e0d4ff38b0eb95
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2024-12-02 23:26:09 +0700

    Thêm logic operator vào Python

---
+++ Python/logicaloperator.py
@@ -0,0 +1,17 @@
+# temp = 20
+# is_raining = True
+# if temp > 35 or temp < 0 or is_raining:
+#     print("Cancelled")
+# else:
+#     print("Let's go")
+
+# temp = 29
+# is_sunny = False
+# if temp >= 28 and is_sunny:
+#     print("Hot and Sunny")
+# elif temp <= 0 and is_sunny:
+#     print("Cold but Sunny")
+# elif 28 > temp > 0 and is_sunny:
+#     print("Warm and Sunny")
+# if temp >= 28 and not is_sunny:
+#     print("Cloudy")
